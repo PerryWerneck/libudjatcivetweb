db8b2e368d712f9939e9fba18e869c0956a5aa60
Perry Werneck <perry.werneck@gmail.com>
2021-04-13 23:37:43 -0300

    Starting implementation of the civetweb module.

--- src/init.cc
+++ src/init.cc
@@ -20,28 +20,63 @@
  #include <udjat/defs.h>
  #include <udjat/module.h>
  #include <udjat/tools/quark.h>
+ #include <udjat/tools/configuration.h>
+ #include <civetweb.h>
 
  using namespace Udjat;
  using namespace std;
 
+ static int log_message(const struct mg_connection *conn, const char *message);
+
+
  class Module : public Udjat::Module {
+ private:
+	struct mg_context *ctx;
+
  public:
 
- 	Module(void *handle) : Udjat::Module(Quark::getFromStatic("civetweb"),handle) {
-
+ 	Module(void *handle) : Udjat::Module(Quark::getFromStatic("civetweb"),handle), ctx(NULL) {
+		mg_init_library(0);
  	};
 
  	virtual ~Module() {
-
+		stop();
+		mg_exit_library();
  	}
 
 	void start() override {
 
+		if(!ctx) {
+
+			const char *options[] = {
+				"listening_ports", 			Config::Value<std::string>("civetweb","listening_ports","8989").c_str(),
+				"request_timeout_ms",		Config::Value<std::string>("civetweb","request_timeout_ms","10000").c_str(),
+				"error_log_file",			Config::Value<std::string>("civetweb","error_log_file","error.log").c_str(),
+				"enable_auth_domain_check",	Config::Value<std::string>("civetweb","enable_auth_domain_check","no").c_str(),
+				NULL
+			};
+
+			// https://github.com/civetweb/civetweb/blob/master/docs/api/mg_start.md
+			struct mg_callbacks callbacks;
+			memset(&callbacks,0,sizeof(callbacks));
+			callbacks.log_message = log_message;
+
+			ctx = mg_start(&callbacks, 0, options);
+			if (ctx == NULL) {
+				throw runtime_error("Cannot start CivetWeb - mg_start failed.");
+			}
+
+		}
 
 	}
 
 	void stop() override {
 
+		if(ctx) {
+			mg_stop(ctx);
+		}
+
+		ctx = NULL;
 	}
 
  };
@@ -50,4 +85,12 @@
  Udjat::Module * udjat_module_init(void *handle) {
 	return new ::Module(handle);
  }
+
+ #pragma GCC diagnostic push
+ #pragma GCC diagnostic ignored "-Wunused-parameter"
+ int log_message(const struct mg_connection *conn, const char *message) {
+	clog << "civetweb\t" << message << endl;
+	return 1;
+ }
+ #pragma GCC diagnostic pop
 
