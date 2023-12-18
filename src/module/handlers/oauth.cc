f510b6a77b97139693b57687abc610dfe80ab3e7
Perry Werneck <perry.werneck@gmail.com>
2023-12-18 09:32:03 -0300

    Implementing authentication.

--- src/module/handlers/oauth.cc
+++ src/module/handlers/oauth.cc
@@ -18,13 +18,14 @@
  */
 
  /**
-  * @brief Implements the pubkey output.
+  * @brief Implements simple oauth2 authenticator.
   *
   */
 
  // References:
 
  //	https://www.tutorialspoint.com/oauth2.0/oauth2.0_obtaining_an_access_token.htm
+ // https://www.freebsd.org/doc/en/articles/pam/pam-essentials.html
 
  #include <config.h>
  #include <udjat/defs.h>
@@ -42,6 +43,17 @@
  #include <sys/socket.h>
  #include <netinet/in.h>
 
+ #if defined(HAVE_PAM)
+	#include <security/pam_appl.h>
+ #endif // HAVE_PAM
+
+ #ifdef _WIN32
+	#include <windows.h>
+ #else
+	#include <sys/types.h>
+	#include <pwd.h>
+ #endif // _WIN32
+
  using namespace std;
 
  #ifdef HAVE_LIBSSL
@@ -51,6 +63,14 @@
  struct AuthenticationToken {
 
 	unsigned char type = 0;
+
+#ifdef _WIN32
+
+#else
+
+	uid_t user = ((uid_t) -1);
+
+#endif // _WIN32
 
 	/// @brief The authenticated user id.
 	unsigned int id = (unsigned int) -1;
@@ -68,6 +88,128 @@
 
  };
  #pragma pack()
+
+#ifdef HAVE_PAM
+	///
+	/// @brief Run PAM conversation request.
+	///
+	/// Reference: <https://www.freebsd.org/doc/en/articles/pam/pam-sample-conv.html>
+	///
+	///
+	static int pam_conversation(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *pwd) {
+
+		debug(__FUNCTION__);
+
+		if (num_msg <= 0 || num_msg > PAM_MAX_NUM_MSG)
+			return (PAM_CONV_ERR);
+
+		*resp = (struct pam_response *) calloc(num_msg, sizeof *resp);
+
+		if(!*resp) {
+			return (PAM_BUF_ERR);
+		}
+
+		for(int f = 0; f < num_msg;f++) {
+
+			debug("style[",f,"=%",msg[f]->msg_style);
+
+			switch(msg[f]->msg_style) {
+			case PAM_PROMPT_ECHO_OFF:
+				resp[f]->resp = strdup((const char *) pwd);
+				break;
+
+			case PAM_PROMPT_ECHO_ON:
+				debug(msg[f]->msg);
+				break;
+
+			case PAM_ERROR_MSG:
+				Logger::String{msg[f]->msg}.error("pam");
+				break;
+
+			case PAM_TEXT_INFO:
+				Logger::String{msg[f]->msg}.info("pam");
+				break;
+
+			default:
+				Logger::String{"Invalid or unexpected conversation message style: ",msg[f]->msg_style}.warning("pam");
+
+			}
+
+
+		}
+
+		return (PAM_SUCCESS);
+
+	}
+
+	static int pam_exec(const char *username, const char *pwd, string &message){
+
+		int state = EINVAL;
+
+	    struct pam_conv local_conversation = {
+	    	pam_conversation,
+	    	(void *) pwd
+		};
+
+		int 			  retval		= 0;
+		pam_handle_t 	* local_auth_handle = NULL; // this gets set by pam_start
+
+#ifdef DEBUG
+		retval = pam_start("common-auth", username, &local_conversation, &local_auth_handle);
+#else
+		retval = pam_start(
+						Config::Value{"oauth2","service-name",Application::Name().c_str()}.c_str(),
+						username,
+						&local_conversation,
+						&local_auth_handle
+					);
+#endif // DEBUG
+
+		if(retval != PAM_SUCCESS) {
+			throw runtime_error(Logger::String{pam_strerror(local_auth_handle, retval), " (rc=", retval, ")"});
+		}
+
+		retval = pam_authenticate(local_auth_handle, 0);
+
+		switch(retval) {
+		case PAM_SUCCESS:
+			Logger::String{username," was authenticated"}.info("pam");
+			state = 0;
+
+			retval = pam_setcred(local_auth_handle,PAM_ESTABLISH_CRED);
+			if(retval != PAM_SUCCESS) {
+				Logger::String{"setcred: ", pam_strerror(local_auth_handle, retval), "(rc=",retval,")"}.error("pam");
+			}
+			break;
+
+		case PAM_AUTH_ERR:
+			Logger::String{username," was not authenticated"}.error("pam");
+			state = EPERM;
+			break;
+
+		case PAM_USER_UNKNOWN:
+			Logger::String{username, ": ", pam_strerror(local_auth_handle, retval), " (rc=",retval,")"}.error("pam");
+			state = ENOENT;
+			message = _("Invalid username");
+			break;
+
+		default:
+			Logger::String{username, ": ", pam_strerror(local_auth_handle, retval), " (rc=",retval,")"}.error("pam");
+			message = pam_strerror(local_auth_handle,retval);
+			state = EINVAL;
+		}
+
+		retval = pam_end(local_auth_handle, retval);
+
+		if (retval != PAM_SUCCESS) {
+			Logger::String{"pam_end(",username,") returned ",retval}.error("pam");
+		}
+
+		return state;
+
+	}
+
+#endif // HAVE_PAM
 
  static void header_send(struct mg_connection *conn, AuthenticationToken &token) {
 
@@ -206,7 +348,11 @@
 			// Reset token.
 			memset(&token,0,sizeof(token));
 
-			{
+			// Authenticate
+			if(request == MimeType::html) {
+
+				string message{_("Access Denied")};
+
 				String username{request["username"]};
 				if(username.empty()) {
 					return login_page(conn,request,token,_("Please, inform username"));
@@ -217,12 +363,51 @@
 					return login_page(conn,request,token,_("Please, inform password"));
 				}
 
+				//
+				// Do the authentication
+				//
+#if defined(HAVE_PAM)
 
+				// Use PAM
+				// https://www.freebsd.org/doc/en/articles/pam/pam-essentials.html
 
+				if(!pam_exec(username.c_str(),password.c_str(),message)) {
+
+					// User was authenticated
+					struct passwd pwd;
+					struct passwd *ppwd = NULL;
+					char buf[1024];
+
+					if (getpwnam_r(username.c_str(), &pwd, buf, sizeof buf, &ppwd)) {
+
+						message = strerror(errno);
+
+					} else {
+
+						// Got user info
+						token.user = pwd.pw_uid;
+
+						String target{request["redirect_uri"]};
+
+						mg_response_header_start(conn, 303);
+						mg_response_header_add(conn, "Location",target.c_str(),target.size());
+						mg_response_header_add(conn, "Content-Length", "0", -1);
+						header_send(conn,token);
+
+						return 303;
+
+					}
+				}
+
+#else
+				// No authentication module
+				message = strerror(ENOTSUP);
+#endif
+
+				// Authentication failed, send login again
+				return login_page(conn,request,token,message.c_str());
 			}
-
-			// Authentication failed, send login again
-			return login_page(conn,request,token,_("Access Denied"));
+			break;
 
 		default:
 			mg_send_http_error(conn, 400, "Invalid request: '%s'", request.path());
